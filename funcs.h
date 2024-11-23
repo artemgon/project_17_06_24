cad05e0a9b8ed15955f8313899e7240c1ec05aff
artemgon <artemhoncharuk123@gmail.com>
2024-11-23 11:40:04 +0200

    Task 1 is done.

--- funcs.h
+++ funcs.h
@@ -1,1 +1,32 @@
 #include "libs.h" 
+
+template <typename T>
+void printFunc(T ** array, int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << *(*(array + i) + j) << " ";
+		}
+		cout << endl;
+	}
+}
+
+template <typename T>
+void addFunc(T** &array, int& rows, int cols, T *array_1)
+{
+	T** new_a = new T * [rows + 1];
+	for (int i = 0; i < rows; i++)
+	{
+		*(new_a + i) = *(array + i);
+	}
+	new_a[rows] = new T[cols];
+	for (int i = 0; i < cols; i++)
+	{
+		*(*(new_a + rows) + i) = *(array_1 + i);
+	}
+	delete[] array;
+	array = new_a;
+	rows++;
+}
